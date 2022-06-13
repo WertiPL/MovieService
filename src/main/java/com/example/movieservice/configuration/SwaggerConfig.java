abb0b56ea9e7dbb8f01a53c019b765c2b0d5eaef
WertiPL <wiktor.rostkowski@hotmail.com>
2022-06-13 20:09:00 +0200

    Swagger

--- src/main/java/com/example/movieservice/configuration/SwaggerConfig.java
+++ src/main/java/com/example/movieservice/configuration/SwaggerConfig.java
@@ -4,9 +4,13 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import java.util.Collections;
 
 @Configuration
 @EnableSwagger2
@@ -18,5 +22,16 @@ public class SwaggerConfig {
                 .paths(PathSelectors.any())
                 .apis(RequestHandlerSelectors.basePackage("com.example.movieservice"))
                 .build();
+    }
+    private ApiInfo createApiInfo() {
+        return new ApiInfo("Rental Service API",
+                "Movie database",
+                "1.00",
+                "https://github.com/WertiPL/MovieService",
+                new Contact("Wiktor Rostkowski", "http://vvrw.eu", "wiktor@vvwi.eu"),
+                "GUI",
+                "",
+                Collections.emptyList()
+        );
     }
 }
